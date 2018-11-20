a40855d75699d682674f6cd9c12200c66567a38d
vaidicreddy <44340239+vaidicreddy@users.noreply.github.com>
2018-11-20 15:57:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ENV=$1
 if [ $ENV = "QA" ]; then
- for i in 'cat ip.txt'
+ for i in `cat ip.txt`
   do
     echo "deploy war file $i:"
     sshpass -p "1234" scp target/ford.war mahesh1@$i:/home/mahesh1/soft/apache-tomcat-8.5.32/webapps
